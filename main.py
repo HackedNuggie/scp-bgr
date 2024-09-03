bccb7dd327c04a861f4318a1811fe7c88640b197
HackedNuggie <119738792+HackedNuggie@users.noreply.github.com>
2024-09-03 22:54:28 +0200

    _

--- main.py
+++ main.py
@@ -1,6 +1,53 @@
 from scp import SCP
+from time import sleep
+import json
+from flask import Flask, redirect, render_template, request, url_for
+from random import randrange
 
+# \u2588 is the ████ characters
+def create_scps(num) -> None:
+    for x in range(895,2000):
+        try:
+            scp = SCP.create_scp(x)
+            with open("persistent/scp.json","a") as f:
+                json.dump(scp.json(),f)
+                f.write(",\n")
+            print(x)
+            if scp.site is None:
+                with open ("persistent/failed.txt","a") as f:
+                    f.write(f"{x} no description\n")
+        except Exception as e:
+            with open("persistent/failed.txt","a") as f:
+                f.write(f"{x}: {e}\n")
+            print(f"{x} failed")
+        #sleep(0.1)
+    print("done")
+
+def test_scp(num):
+    scp = SCP.create_scp(num)
+    print(scp)
 
 if __name__ == '__main__':
-    scp1 = SCP.create_scp(7432)
-    print(scp1)
+    app = Flask(__name__)
+    @app.route("/scps")
+    def scps():
+        with open("persistent/scp.json","r") as f:
+            scps = json.load(f)
+            print(scps)
+    
+    
+    @app.route("/scps/") # <int:num>")
+    def scp(num = 682):
+        # num -= 100
+        
+        print(num)
+        with open("persistent/scp.json","r") as f:
+            scps = json.load(f)
+        x = True
+        while x == True:
+            num = randrange(0,len(scps))-100
+            #print(scps[num]["image"])
+            if scps[num]["image"]:
+                x = False
+        return render_template("index.html",scp=scps[num])
+    app.run(host='0.0.0.0', port=8080, debug=True)
--- scp.py
+++ scp.py
@@ -4,9 +4,9 @@ import re
 
 
 class SCP:
-    def __init__(self, number, name, site, cont_class, disr_class, risk_class, image):
+    def __init__(self, number, description, site, cont_class, disr_class, risk_class, image):
         self.number = number
-        self.name = name
+        self.description = description
         self.site = site
         self.containment_class = cont_class
         self.disruption_class = disr_class 
@@ -19,33 +19,50 @@ class SCP:
                 f"Containment Class: {self.containment_class}\n"
                 f"Disruption Class: {self.disruption_class}\n"
                 f"Risk Class: {self.risk_class}\n"
+                f"Description: {self.description}\n"
                 f"Image: {self.image}")
+    
+    def json(self):
+        return {
+            "number": self.number,
+            "site": self.site,
+            "containment_class": self.containment_class,
+            "disruption_class": self.disruption_class,
+            "risk_class": self.risk_class,
+            "description": self.description,
+            "image": self.image
+        }
+
     @staticmethod
     def create_scp(num):
         url = f'https://scp-wiki.wikidot.com/scp-{num}'
         soup = BeautifulSoup(requests.get(url).content, 'html.parser')
         
         txt = "\n".join(soup.text.splitlines()[215:])
-        
+        """
         with open("text.txt","w",encoding="utf-8") as f:
             f.write(txt)
-        
+        """
         number = num
 
         # check if there's a containment site
-        site_search = re.search(r'(?:contained|located|held|situated|occupies|placed)\s+(?:in|on|at|a|within|by|near|adjacent to|around)\s+(.*)', txt)
+        #site_search = re.search(r'(?:contained|located|held|housed|situated|stored|kept|occupies|placed)\s+(?:in|on|at|a|within|by|near|adjacent to|around)\s+(.*)', txt) # I might be a dumbass
+        site_search = re.search(r'(?:Special Containment Procedures:|Special Containment Procedure:|Special Containment Procedures|Special Containment Procedure)\s*([^\n]+)', txt, re.IGNORECASE)
         site = site_search.group(1).strip() if site_search else None
 
+
+        desc_search = re.search(r"(?:description:|description )\s*([^\n]+)", txt, re.IGNORECASE)
+        desc = desc_search.group(1).strip() if desc_search else None
         # find class (keter, euclid, afe)
-        cont_class_search = re.search(r"(Containment Class|Object Class):\s*([^\n]+)", txt)
+        cont_class_search = re.search(r"(Containment Class|Object Class):\s*([^\n]+)", txt, re.IGNORECASE)
         cont_class = cont_class_search.group(2).strip() if cont_class_search else None
 
         # get disruption class (dark, vlam, notice)
-        disr_class_search = re.search(r"Disruption Class:\s*([^\n]+)", txt)
+        disr_class_search = re.search(r"Disruption Class:\s*([^\n]+)", txt, re.IGNORECASE)
         disr_class = disr_class_search.group(1).strip() if disr_class_search else None
-
+        
         # get risk class (notice, caution, warning)
-        risk_class_search = re.search(r"Risk Class:\s*([^\n]+)", txt)
+        risk_class_search = re.search(r"Risk Class:\s*([^\n]+)", txt, re.IGNORECASE)
         risk_class = risk_class_search.group(1).strip() if risk_class_search else None
 
         # find all image divs
@@ -55,8 +72,8 @@ class SCP:
             # check in the divs for images and descriptions
             for x in image_divs:
                 image = x.find('img')["src"]
-                img_desc = x.find('div').find('p')
+                img_desc = x.find('div').find(['p','tt'])
                 images.append([image, img_desc.text])
         else: images = None
 
-        return SCP(number=num, name=None, site=site, cont_class=cont_class, disr_class=disr_class, risk_class=risk_class, image=images)
+        return SCP(number=num, description=desc, site=site, cont_class=cont_class, disr_class=disr_class, risk_class=risk_class, image=images)
